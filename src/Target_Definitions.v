de42d914a6c8f255e4e0c91d93395aab6f5169e4
Phillip Mates <phillipmates@gmail.com>
2013-01-30 15:25:05 -0500

    removed trailing whitespace

--- src/Target_Definitions.v
+++ src/Target_Definitions.v
@@ -64,15 +64,15 @@ Fixpoint open_te_rec (K : nat) (U : typ) (e : trm) {struct e} : trm :=
   | trm_false     => trm_false
   | trm_pair v1 v2 => trm_pair  (open_te_rec K U v1)  (open_te_rec K U v2)
   | trm_abs t e1  => trm_abs  (open_tt_rec (S K) U t)  (open_te_rec (S K) U e1)
-  | trm_if v e1 e2 => trm_if (open_te_rec K U v) 
+  | trm_if v e1 e2 => trm_if (open_te_rec K U v)
                              (open_te_rec K U e1)
                              (open_te_rec K U e2)
   | trm_let_fst v e => trm_let_fst (open_te_rec K U v)
-                                   (open_te_rec K U e) 
+                                   (open_te_rec K U e)
   | trm_let_snd v e => trm_let_snd (open_te_rec K U v)
-                                   (open_te_rec K U e) 
+                                   (open_te_rec K U e)
   | trm_app e1 t e2 => trm_app  (open_te_rec K U e1)
-                                (open_tt_rec K U t) 
+                                (open_tt_rec K U t)
                                 (open_te_rec K U e2)
   end.
 
@@ -88,13 +88,13 @@ Fixpoint open_ee_rec (k : nat) (f : trm) (e : trm) {struct e} : trm :=
   | trm_false     => trm_false
   | trm_pair v1 v2 => trm_pair  (open_ee_rec k f v1)  (open_ee_rec k f v2)
   | trm_abs t e1  => trm_abs t (open_ee_rec (S k) f e1)
-  | trm_if v e1 e2 => trm_if (open_ee_rec k f v) 
+  | trm_if v e1 e2 => trm_if (open_ee_rec k f v)
                              (open_ee_rec k f e1)
                              (open_ee_rec k f e2)
   | trm_let_fst v e => trm_let_fst (open_ee_rec k f v)
-                                   (open_ee_rec (S k) f e) 
+                                   (open_ee_rec (S k) f e)
   | trm_let_snd v e => trm_let_snd (open_ee_rec k f v)
-                                   (open_ee_rec (S k) f e) 
+                                   (open_ee_rec (S k) f e)
   | trm_app e1 t e2 => trm_app  (open_ee_rec k f e1)
                                 t
                                 (open_ee_rec k f e2)
@@ -105,7 +105,7 @@ Definition open_ee t u := open_ee_rec 0 u t.
 (** Notation for opening up binders with type or term variables *)
 
 (* changing type vars in a term *)
-Definition open_te_var e X := (open_te e (typ_fvar X)). 
+Definition open_te_var e X := (open_te e (typ_fvar X)).
 (* changing type vars in a type *)
 Definition open_tt_var T X := (open_tt T (typ_fvar X)).
 (* changing a term var in a term *)
@@ -121,7 +121,7 @@ Inductive type : typ -> Prop :=
   | type_var : forall X,
       type (typ_fvar X)
   | type_arrow : forall L T1 T2,
-      (forall X, X \notin L -> 
+      (forall X, X \notin L ->
         type (open_tt_var T1 X) /\ type (open_tt_var T2 X)) ->
       type (typ_arrow T1 T2).
 
@@ -135,11 +135,11 @@ Inductive term : trm -> Prop :=
       term e2 ->
       term (trm_if v e1 e2)
   | term_let_fst : forall L v e,
-      value v -> 
+      value v ->
       (forall x, x \notin L -> term (open_ee_var e x)) ->
       term (trm_let_fst v e)
   | term_let_snd : forall L v e,
-      value v -> 
+      value v ->
       (forall x, x \notin L -> term (open_ee_var e x)) ->
       term (trm_let_snd v e)
   | term_app : forall T v1 v2,
@@ -155,8 +155,8 @@ with value : trm -> Prop :=
   | value_false : value trm_false
   | value_pair : forall v1 v2,
       value v1 -> value v2 -> value (trm_pair v1 v2)
-  | value_abs  : forall L T e1, 
-      (forall x X, x \notin L -> X \notin L -> 
+  | value_abs  : forall L T e1,
+      (forall x X, x \notin L -> X \notin L ->
         type (open_tt_var T X) /\ term (open_te_var (open_ee_var e1 x) X)) ->
       value (trm_abs T e1).
 
@@ -170,10 +170,10 @@ Definition star := tt.        (* base kind: '*' *)
   all the type variables of T must be bound via a
   subtyping relation in E. This predicates implies
   that T is a type *)
- 
+
 (* Delta |- tau *)
 Inductive wft : env_type -> typ -> Prop :=
-  | wft_bool : forall D, 
+  | wft_bool : forall D,
       wft D typ_bool
   | wft_pair : forall D T1 T2,
       wft D T1 ->
@@ -181,9 +181,9 @@ Inductive wft : env_type -> typ -> Prop :=
       wft D (typ_pair T1 T2)
   | wft_var : forall D X,
       binds X star D ->
-      wft D (typ_fvar X) 
+      wft D (typ_fvar X)
   | wft_arrow : forall L D T1 T2,
-      (forall X, X \notin L -> 
+      (forall X, X \notin L ->
         wft (D & X ~ star) (open_tt_var T1 X) /\
         wft (D & X ~ star) (open_tt_var T2 X)) ->
       wft D (typ_arrow T1 T2).
@@ -203,7 +203,7 @@ Inductive okt : env_type -> env_term -> Prop :=
 
 (** Typing relation *)
 (* Delta;Gamma |- e:T *)
-(* NOTE: Might need to enforce value restrictions 
+(* NOTE: Might need to enforce value restrictions
          we need to be able to prove: D G |- e : T -> term e *)
 Inductive typing : env_type -> env_term -> trm -> typ -> Prop :=
   | typing_var : forall D G x T,
@@ -212,19 +212,19 @@ Inductive typing : env_type -> env_term -> trm -> typ -> Prop :=
       typing D G (trm_fvar x) T
   | typing_true : forall D G,
      okt D G ->
-     typing D G trm_true typ_bool 
+     typing D G trm_true typ_bool
   | typing_false : forall D G,
      okt D G ->
-     typing D G trm_false typ_bool 
+     typing D G trm_false typ_bool
   | typing_pair : forall D G v1 v2 T1 T2,
     typing D G v1 T1 ->
     typing D G v2 T2 ->
     typing D G (trm_pair v1 v2) (typ_pair T1 T2)
   | typing_abs : forall L D G e T1 T2,
       (forall x X, x \notin L -> X \notin L ->
-        typing (D & X ~ star) 
+        typing (D & X ~ star)
                (G & x ~ (open_tt_var T1 X))
-               (open_te_var (open_ee_var e x) X) 
+               (open_te_var (open_ee_var e x) X)
                (open_tt_var T2 X)) ->
       typing D G (trm_abs T1 e) (typ_arrow T1 T2)
   | typing_if : forall D G v e1 e2 T,
@@ -234,12 +234,12 @@ Inductive typing : env_type -> env_term -> trm -> typ -> Prop :=
     typing D G (trm_if v e1 e2) T
   | typing_let_fst : forall L D G v e T1 T2 T,
     typing D G v (typ_pair T1 T2) ->
-    (forall x, x \notin L -> 
+    (forall x, x \notin L ->
       typing D (G & x ~ T1) (open_ee_var e x) T) ->
     typing D G (trm_let_fst v e) T
   | typing_let_snd : forall L D G v e T1 T2 T,
     typing D G v (typ_pair T1 T2) ->
-    (forall x, x \notin L -> 
+    (forall x, x \notin L ->
       typing D (G & x ~ T2) (open_ee_var e x) T) ->
     typing D G (trm_let_snd v e) T
   | typing_app : forall D G v1 v2 T T1 T2,
@@ -278,12 +278,12 @@ Inductive red : trm -> trm -> Prop :=
 (** Our goal is to prove preservation and progress *)
 
 Definition preservation := forall D G e e' T,
-  typing D G e T -> 
-  red e e' -> 
+  typing D G e T ->
+  red e e' ->
   typing D G e' T.
 
 Definition progress := forall e T,
-  typing empty empty e T -> 
-     value e 
+  typing empty empty e T ->
+     value e
   \/ exists e', red e e'.
 
