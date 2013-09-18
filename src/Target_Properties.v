9b230d606b5441efbb52a74c5b420b28472eae7f
James T. Perconti <jtpercon@ccs.neu.edu>
2013-09-18 13:41:28 -0400

    cleaned up a bunch of proofs about target types

--- src/Target_Properties.v
+++ src/Target_Properties.v
@@ -25,13 +25,9 @@ Lemma t_wft_weaken_generalized : forall G T E F,
   t_wft (E & G) T -> ok (E & F & G) ->
   t_wft (E & F & G) T.
 Proof.
-  (* TODO: I don't know what half the tactics in this proof are;
-   * we should figure that out because they seem pretty useful -JTP *)
   intros. gen_eq K: (E & G). gen E F G.
   induction H; intros; subst; eauto.
-  (* case: var *)
   apply* t_wft_var. apply* binds_weaken.
-  (* case: all *)
   apply_fresh* t_wft_arrow as Y. apply_ih_bind* H0. apply_ih_bind* H2.
 Qed.
 
@@ -55,12 +51,9 @@ Lemma open_tt_rec_t_type_core : forall t j t' t'' i, i <> j ->
   t = open_tt_rec i t'' t.
 Proof.
   induction t; intros; simpl in *; inversion H0; f_equal*.
-  (* is there a tactic that can do this automatically from here? *)
-  case_eq (EqNat.beq_nat i n); intros; auto.
-  apply EqNat.beq_nat_true in H1. subst.
-  case_eq (EqNat.beq_nat j n); intros; auto.
-  apply EqNat.beq_nat_true in H1. subst. destruct* H.
-  rewrite H1 in H0. simpl in H0. rewrite* NPeano.Nat.eqb_refl in H0.
+  case_if*. apply EqNat.beq_nat_true in H1. subst.
+  case_if*. apply EqNat.beq_nat_true in H1. false.
+  simpl in *. case_if*. apply EqNat.beq_nat_false in H3. false.
 Qed.
 
 Lemma open_tt_rec_t_type : forall t t' n, t_type t ->
@@ -76,10 +69,8 @@ Lemma subst_tt_open_tt_rec : forall t t' n d, wfenv t_type d ->
   open_tt_rec n (subst_tt d t') (subst_tt d t).
 Proof.
   induction t; intros; auto; simpl; try f_equal; auto. 
-  destruct (EqNat.beq_nat n0 n); auto.
-  case_eq (get v d); intros; auto.
-    assert (t_type t). apply* (wfenv_binds t_type d v).
-    apply* open_tt_rec_t_type.
+  case_if*. cases* (get v d).
+  apply* open_tt_rec_t_type. apply* (wfenv_binds t_type d v).
 Qed.
 
 Lemma subst_tt_open_tt : forall t t' d, wfenv t_type d ->
@@ -101,8 +92,8 @@ Lemma subst_tt_intro_rec : forall X t t' n,
   open_tt_rec n t' t = subst_tt (X ~ t') (open_tt_rec n (t_typ_fvar X) t).
 Proof.
   intros X t. gen X. induction t; intros; auto; simpl in *; try f_equal; auto.
-  destruct (EqNat.beq_nat n0 n); auto. simpl. rewrite get_single. rewrite* If_l.
-  rewrite get_single. rewrite* If_r.
+  cases_if*. simpl. rewrite get_single. cases_if*.
+  rewrite get_single. cases_if*. false. apply* notin_same.
 Qed.
 
 Lemma subst_tt_intro : forall X t t',
@@ -112,12 +103,12 @@ Proof.
   intros. apply* subst_tt_intro_rec.
 Qed.  
 
-(* this lemma is nice and all but I'm not sure it actually helps -JTP *)
+(* XXX: unused *)
 Lemma subst_tt_preserves_t_type : forall d t,
   t_type t -> wfenv t_type d -> t_type (subst_tt d t).
 Proof.
   induction 1; intros; simpl; auto.
-  case_eq (get x d); intros. eapply wfenv_binds; eassumption. auto.
+  cases* (get x d). eapply wfenv_binds; eassumption.
   apply_fresh t_type_arrow as X;
   replace (t_typ_fvar X) with (subst_tt d (t_typ_fvar X));
   try rewrite <- subst_tt_open_tt; auto; simpl;
@@ -131,17 +122,15 @@ Lemma subst_tt_preserves_t_wft_generalized : forall D D' t X t',
   X # (D & D') -> t_wft (D & D') t' -> t_wft (D & X ~ star & D') t ->
   t_wft (D & D') (subst_tt (X ~ t') t).
 Proof.
-  intros. remember (D & X ~ star & D') as DXD'. gen D. gen X. gen D'.
+  intros. gen_eq DXD' : (D & X ~ star & D'). gen D X D'.
   induction H1; intros; subst; simpl; auto.
-  rewrite get_single. destruct (classicT (X = X0)).
-    auto.
-    apply t_wft_var; auto. eapply binds_subst; eauto.
+  rewrite get_single. cases_if*.
+    apply* t_wft_var. apply* binds_subst.
   apply_fresh t_wft_arrow as Y;
-  rewrite subst_tt_open_tt_var; auto; try rewrite <- concat_assoc.
-  apply H0; try rewrite concat_assoc; auto. apply* t_wft_weaken.
-  apply wfenv_single. eapply t_wft_implies_t_type. eauto.
-  apply H2; try rewrite concat_assoc; auto. apply* t_wft_weaken.
-  apply wfenv_single. eapply t_wft_implies_t_type. eauto.
+  rewrite* subst_tt_open_tt_var. apply_ih_bind* H0. apply* t_wft_weaken.
+  apply wfenv_single. apply* t_wft_implies_t_type.
+  apply_ih_bind* H2. apply* t_wft_weaken.
+  apply wfenv_single. apply* t_wft_implies_t_type.
 Qed.
 
 Lemma subst_tt_preserves_t_wft : forall D t X t',
@@ -149,22 +138,17 @@ Lemma subst_tt_preserves_t_wft : forall D t X t',
   t_wft D (subst_tt (X ~ t') t).
 Proof.
   intros.
-  replace D with (D & empty); replace D with (D & empty) in H0;
-    try apply concat_empty_r.
-  replace (D & X ~ star) with (D & X ~ star & empty) in H1;
-    try apply concat_empty_r.
-  apply subst_tt_preserves_t_wft_generalized; auto.
+  rewrite <- (concat_empty_r D). rewrite <- (concat_empty_r D) in H0.
+  rewrite <- (concat_empty_r (D & X ~ star)) in H1.
+  apply* subst_tt_preserves_t_wft_generalized.
 Qed.
 
 Lemma t_wft_arrow_apply : forall D t1 t2 t,
   t_wft D (t_typ_arrow t1 t2) -> t_wft D t ->
   t_wft D (open_tt t2 t).
 Proof.
-  intros. inversion H. subst.
-  pick_fresh X.
-  assert (t_wft (D & X ~ star) (open_tt t2 (t_typ_fvar X))); auto.
-  rewrite* (subst_tt_intro X).
-  apply subst_tt_preserves_t_wft; try rewrite map_single; auto.
+  intros. inverts H.
+  pick_fresh X. rewrite* (subst_tt_intro X). apply* subst_tt_preserves_t_wft.
 Qed.
 
 (* Regularity of t_typing *) 
