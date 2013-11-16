0833811362f6590294ba3cff11e35ec47c206bee
Phillip Mates <phillipmates@gmail.com>
2013-11-15 19:42:26 -0500

    (untested) fixpoint version of cps_term_trans w/o type inferencer

--- src/Cps_Trans.v
+++ src/Cps_Trans.v
@@ -1,6 +1,7 @@
 Require Import LibWfenv Core_Infrastructure
                Source_Definitions Target_Definitions
                Source_Properties Target_Properties.
+Require Import CpdtTactics.
 Require Import Recdef.
 
 (* for applications where the type argument is irrelevant *)
@@ -39,34 +40,84 @@ match e with
   let s := get v G in
   match s with
   | Some s' =>
+    (* λ [σ] k:s+ → α . k [_] x *)
     (t_trm_abs (t_typ_arrow (cps_type_trans s') (t_typ_bvar 0))
                (t_trm_app (t_trm_bvar 0) dummy_type (t_trm_fvar v)))
   | None => trm_bad
   end
 | s_trm_true =>
+  (* λ [σ] k:bool+ → α . k [_] true *)
   (t_trm_abs (t_typ_arrow (cps_type_trans s_typ_bool) (t_typ_bvar 0))
                      (t_trm_app (t_trm_bvar 0) dummy_type t_trm_true))
 | s_trm_false =>
+  (* λ [σ] k:bool+ → α . k [_] false *)
   (t_trm_abs (t_typ_arrow (cps_type_trans s_typ_bool) (t_typ_bvar 0))
                      (t_trm_app (t_trm_bvar 0) dummy_type t_trm_false))
 | s_trm_abs s1 bdy =>
   (* TODO: write a type inferencer to get s2 *)
   let s2 := s_dummy_type in
   let x := var_gen (fv_ee source bdy) in
+  let v := cps_term_trans (G & x ~ s1) (s_open_ee_var bdy x) in
+  (* λ [α] k:((σ1 → σ2)+ → α) . *)
   (t_trm_abs (t_typ_arrow (cps_type_trans (s_typ_arrow s1 s2)) (t_typ_bvar 0))
-                   (t_trm_app (t_trm_bvar 0) dummy_type
-                              (t_trm_abs 
-                                (t_typ_pair (cps_type_trans s1) (t_typ_arrow (cps_type_trans s2) (t_typ_bvar 0)))
-                                (* TODO: check let projection bvar indices *)
-                                (t_trm_let_fst (t_trm_bvar 0) (* project out x and bind to 0 *)
-                                  (t_trm_let_snd (t_trm_bvar 1) (* project out k' and bind to 0 *)
-                                    (t_trm_app (cps_term_trans (G & x ~ s1) (s_open_ee_var bdy x)) (t_typ_bvar 0) (t_trm_bvar 0)))))))
-(* TODO: finish *)
-| s_trm_app e1 e2 => trm_bad
-| s_trm_if e1 e2 e3 => trm_bad
+    (* k [_] ... *)
+    (t_trm_app (t_trm_bvar 0) dummy_type
+      (* (λ [β] pr:(σ1+ ⅹ (σ2+ → β)) . *)
+      (t_trm_abs (t_typ_pair (cps_type_trans s1) (t_typ_arrow (cps_type_trans s2) (t_typ_bvar 0)))
+        (* TODO: check let projection bvar indices *)
+        (* let y = fst pr in let k' = snd pr in *)
+        (t_trm_let_fst (t_trm_bvar 0)
+          (t_trm_let_snd (t_trm_bvar 1)
+            (* (v [β] k') *)
+            (t_trm_app v (t_typ_bvar 0) (t_trm_bvar 0)))))))
+| s_trm_app e1 e2 =>
+  (* TODO: s2 -> s from type inference e1 *)
+  let s := s_dummy_type in
+  let s2 := s_dummy_type in
+  let s_arr_plus := (cps_type_trans (s_typ_arrow s2 s)) in
+  let v1 := (cps_term_trans G e1) in
+  let v2 := (cps_term_trans G e2) in
+  (t_trm_abs
+    (* k : s+ -> α *)
+    (t_typ_arrow (cps_type_trans s) (t_typ_bvar 0))
+    (t_trm_app v1 (t_typ_bvar 0)
+      (* λ [_] x1:(σ2→σ)+ . *)
+      (t_trm_abs s_arr_plus
+        (* v1 [σ] ... *)
+        (t_trm_app v2 (t_typ_bvar 1)
+          (* λ [_] x2:σ2+ . *)
+          (t_trm_abs (cps_type_trans s2)
+            (* x1 [σ] (x2, k) *)
+            (t_trm_app (t_trm_bvar 1) (t_typ_bvar 2)
+              (t_trm_pair (t_trm_bvar 0) (t_trm_bvar 2))))))))
+
+| s_trm_if e e1 e2 =>
+  (* TODO: s from type inference of e2 *)
+  let s_plus := (cps_type_trans s_dummy_type) in
+  let v := (cps_term_trans G e) in
+  let v1 := (cps_term_trans G e1) in
+  let v2 := (cps_term_trans G e2) in
+  (* λ [α] k:(σ+ → α) . ...*)
+  (t_trm_abs (t_typ_arrow s_plus (t_typ_bvar 0))
+    (* v [σ] ... *)
+    (t_trm_app v (t_typ_bvar 0)
+      (* λ [_] x:bool . if x then (v1 [α] k) else (v2 [α] k) *)
+      (t_trm_abs t_typ_bool
+        (t_trm_if (t_trm_bvar 0)
+          (t_trm_app v1 (t_typ_bvar 0) (t_trm_bvar 1))
+          (t_trm_app v2 (t_typ_bvar 0) (t_trm_bvar 1))))))
+
+
 | _ => trm_bad
 end.
+(* abs case *)
 intros. simpl. rewrite -> (@s_var_trm_size_open (var_gen (fv_ee source bdy)) bdy). auto.
+(* if case *)
+crush.
+crush.
+crush.
+(* app case *)
+crush. crush.
 Defined.
 
 Inductive cps_trans (G:env_term) (e:trm) (s:typ) (m:trm) : Prop :=
@@ -94,5 +145,4 @@ Inductive cps_trans (G:env_term) (e:trm) (s:typ) (m:trm) : Prop :=
               (t_trm_let_fst (t_trm_bvar 0) (* let x = fst p in *)
                 (t_trm_let_snd (t_trm_bvar 1) (* let k' = snd p in *)
                   (t_trm_app u (*B*)(t_typ_bvar 0) (*k'*)(t_trm_bvar 0))))))).
- (* TODO last line is wrong, need to turn x's in u into bvars *)
- (* TODO app, if *).
+ (* TODO last line is wrong, need to turn x's in u into bvars *).
